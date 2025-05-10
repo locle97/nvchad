625b8aea8afcd2fcb3e28ae21458ba79c879402c
Loc Le <lpl212757@gmail.com>
2025-05-10 22:55:06 +0700

    setup lspconfig

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,70 @@
 require("nvchad.configs.lspconfig").defaults()
 
+local lspconfig = require "lspconfig"
+local on_init = require("nvchad.lsp.utils").on_init
+
 local servers = { "html", "cssls", "angularls", "omnisharp" }
 vim.lsp.enable(servers)
+
+local pid = vim.fn.getpid()
+local util = require "lspconfig/util"
+
+lspconfig.omnisharp.setup {
+    cmd = {
+        "omnisharp",
+        "--languageserver",
+        "--hostPID",
+        tostring(pid),
+    },
+    on_init = on_init,
+    settings = {
+        FormattingOptions = {
+            -- Enables support for reading code style, naming convention and analyzer
+            -- settings from .editorconfig.
+            EnableEditorConfigSupport = true,
+            -- Specifies whether 'using' directives should be grouped and sorted during
+            -- document formatting.
+            OrganizeImports = true,
+        },
+        RoslynExtensionsOptions = {
+            -- Enables support for roslyn analyzers, code fixes and rulesets.
+            EnableAnalyzersSupport = false,
+            -- Enables support for showing unimported types and unimported extension
+            -- methods in completion lists. When committed, the appropriate using
+            -- directive will be added at the top of the current file. This option can
+            -- have a negative impact on initial completion responsiveness,
+            -- particularly for the first few completion sessions after opening a
+            -- solution.
+            EnableImportCompletion = false,
+            -- Only run analyzers against open files when 'enableRoslynAnalyzers' is
+            -- true
+            AnalyzeOpenDocumentsOnly = false,
+        },
+    },
+    root_dir = util.root_pattern("*.sln", "*.csproj"),
+}
+
+-- If you are using mason.nvim, you can get the ts_plugin_path like this
+local mason_registry = require('mason-registry')
+local vue_language_server_path = mason_registry.get_package('vue-language-server'):get_install_path() .. '/node_modules/@vue/language-server'
+
+lspconfig.html.setup {
+    on_init = on_init,
+    filetypes = { 'html', 'vue', 'jsx'},
+}
+
+lspconfig.ts_ls.setup {
+    init_options = {
+        plugins = {
+            {
+                name = '@vue/typescript-plugin',
+                location = vue_language_server_path,
+                languages = { 'vue' },
+            },
+        },
+    },
+    filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+}
+
 
 -- read :h vim.lsp.config for changing options of lsp servers 
